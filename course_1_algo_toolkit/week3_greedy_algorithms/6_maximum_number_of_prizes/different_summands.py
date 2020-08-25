1a744421dfb7e60dc65f38e594f70d5893bb0211
Brucewuzhang <brucezhangwu@gmail.com>
2020-08-25 22:06:46 +0800

    course1 week 3 exe 6 and exe 7

--- course_1_algo_toolkit/week3_greedy_algorithms/6_maximum_number_of_prizes/different_summands.py
+++ course_1_algo_toolkit/week3_greedy_algorithms/6_maximum_number_of_prizes/different_summands.py
@@ -1,10 +1,28 @@
 # Uses python3
 import sys
 
+
 def optimal_summands(n):
     summands = []
-    #write your code here
+    # write your code here
+    # 1 + 2 + ... + k <= n < 1 + 2 + ... + k + k + 1
+    # 0.5 * k * (K + 1) <=n < 0.5 * (k+1) * ( k + 2)
+    if n == 0:
+        return 0
+    current_prize = 1
+    summands.append(1)
+    n -= current_prize
+    current_prize += 1
+    while n != 0:
+        if n < current_prize:
+            summands[-1] = summands[-1] + n
+            return summands
+        else:
+            summands.append(current_prize)
+            n -= current_prize
+            current_prize += 1
     return summands
+
 
 if __name__ == '__main__':
     input = sys.stdin.read()
--- course_1_algo_toolkit/week3_greedy_algorithms/7_maximum_salary/largest_number.py
+++ course_1_algo_toolkit/week3_greedy_algorithms/7_maximum_salary/largest_number.py
@@ -1,17 +1,31 @@
-#Uses python3
+# Uses python3
 
 import sys
 
+
+def IsGreaterOrEqual(digits, maxdigits):
+    new_digits = digits + maxdigits
+    new_max = maxdigits + digits
+    return new_digits > new_max
+
+
 def largest_number(a):
-    #write your code here
+    # write your code here
     res = ""
-    for x in a:
-        res += x
+    while len(a) > 0:
+        maxdigits = a[0]
+        max_index = 0
+        for i, digits in enumerate(a[1:], start=1):
+            if IsGreaterOrEqual(digits, maxdigits):
+                maxdigits = digits
+                max_index = i
+        res += maxdigits
+        a.pop(max_index)
     return res
+
 
 if __name__ == '__main__':
     input = sys.stdin.read()
     data = input.split()
     a = data[1:]
     print(largest_number(a))
-    
