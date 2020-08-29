cf32a1056e617cd657731f507e845f1d9708412d
Brucewuzhang <brucezhangwu@gmail.com>
2020-08-29 19:37:23 +0800

    course 1 week 4 exe 4 exe 5

--- course_1_algo_toolkit/week4_divide_and_conquer/4_number_of_inversions/inversions.py
+++ course_1_algo_toolkit/week4_divide_and_conquer/4_number_of_inversions/inversions.py
@@ -1,6 +1,7 @@
 # Uses python3
 import sys
 
+
 def get_number_of_inversions(a, b, left, right):
     number_of_inversions = 0
     if right - left <= 1:
@@ -8,11 +9,37 @@ def get_number_of_inversions(a, b, left, right):
     ave = (left + right) // 2
     number_of_inversions += get_number_of_inversions(a, b, left, ave)
     number_of_inversions += get_number_of_inversions(a, b, ave, right)
-    #write your code here
+    # write your code here
+    pos = right - 1
+    right_0 = ave
+    right_1 = right
+    left_1 = ave
+    while right_0 - left > 0 and right_1 - left_1 > 0:
+        if a[right_0 - 1] > a[right_1 - 1]:
+            number_of_inversions += right_1 - left_1
+            b[pos] = a[right_0 - 1]
+            right_0 -= 1
+        else:
+            b[pos] = a[right_1 - 1]
+            right_1 -= 1
+        pos -= 1
+    if right_0 - left > 0:
+        for i in range(right_0 - 1, left - 1, -1):
+            b[pos] = a[i]
+            pos -= 1
+    else:
+        for i in range(right_1 - 1, left_1 - 1, -1):
+            b[pos] = a[i]
+            pos -= 1
+    for i, v in enumerate(b[left: right], start=left):
+        a[i] = v
     return number_of_inversions
+
 
 if __name__ == '__main__':
     input = sys.stdin.read()
+#     input = """5
+# 2 3 9 2 9"""
     n, *a = list(map(int, input.split()))
     b = n * [0]
     print(get_number_of_inversions(a, b, 0, len(a)))
--- course_1_algo_toolkit/week4_divide_and_conquer/5_organizing_a_lottery/points_and_segments.py
+++ course_1_algo_toolkit/week4_divide_and_conquer/5_organizing_a_lottery/points_and_segments.py
@@ -1,10 +1,55 @@
 # Uses python3
 import sys
 
+
+def x_less_n_left(starts, x):
+    left = 0
+    right = len(starts)
+    while left < right:
+        m = (left + right) // 2
+        if starts[m] <= x:
+            left = m + 1
+        else:
+            right = m - 1
+    try:
+        if starts[left] <= x:
+            return len(starts) - left - 1
+        else:
+            return len(starts) - left
+    except:
+        return 0
+
+
+def x_greater_n_right(ends, x):
+    left = 0
+    right = len(ends)
+    while left < right:
+        m = (left + right) // 2
+        if ends[m] < x:
+            left = m + 1
+        else:
+            right = m - 1
+    try:
+        if ends[left] < x:
+            return left + 1
+        else:
+            return left
+    except:
+        return len(ends)
+
+
 def fast_count_segments(starts, ends, points):
     cnt = [0] * len(points)
-    #write your code here
+    l_total = len(starts)
+    # write your code here
+    starts.sort()
+    ends.sort()
+    for i, x in enumerate(points):
+        m = x_less_n_left(starts, x)
+        n = x_greater_n_right(ends, x)
+        cnt[i] = l_total - m - n
     return cnt
+
 
 def naive_count_segments(starts, ends, points):
     cnt = [0] * len(points)
@@ -14,15 +59,21 @@ def naive_count_segments(starts, ends, points):
                 cnt[i] += 1
     return cnt
 
+
 if __name__ == '__main__':
     input = sys.stdin.read()
+#     input = """3 2
+# 0 5
+# -3 2
+# 7 10
+# 1 6"""
     data = list(map(int, input.split()))
     n = data[0]
     m = data[1]
     starts = data[2:2 * n + 2:2]
-    ends   = data[3:2 * n + 2:2]
+    ends = data[3:2 * n + 2:2]
     points = data[2 * n + 2:]
-    #use fast_count_segments
-    cnt = naive_count_segments(starts, ends, points)
+    # use fast_count_segments
+    cnt = fast_count_segments(starts, ends, points)
     for x in cnt:
         print(x, end=' ')
